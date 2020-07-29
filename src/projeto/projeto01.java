1de965dbe758502dc3500dd8e99e76b7f09c5c10
Pedro-Luigi <pedroluigi07@gmail.com>
2020-07-28 21:59:46 -0300

    Qualquer coisa só pra testar!

--- src/projeto/projeto01.java
+++ src/projeto/projeto01.java
@@ -1,10 +1,20 @@
 package projeto;
 
+import java.util.Scanner;
+
 public class projeto01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Scanner ler= new Scanner (System.in);
+		int[] casa=new int [3];
+		for (int x=1; x<casa.length;x++) {
+			casa[x]=+1;
+		}
+		System.out.println(casa.length);
+		System.out.println("Você pode escolher uma casa de 1 a 3, qual você quer?");
+		int esolha=ler.nextInt();
+		
 	}
 
 }
